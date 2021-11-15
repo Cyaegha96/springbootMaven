6ddcfc749e9533a3b329c48c6c705dd52175203e
Cyaegha96 <zazae51@gmail.com>
2021-11-15 18:57:07 +0900

    application.properties test

--- src/main/java/test/SpringbootApplication.java
+++ src/main/java/test/SpringbootApplication.java
@@ -3,6 +3,7 @@ package test;
 import java.util.Arrays;
 import java.util.Collection;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -10,7 +11,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
+@PropertySource("classpath:your_external.properties")
 @SpringBootApplication
 public class SpringbootApplication {
 
@@ -21,11 +24,11 @@ public class SpringbootApplication {
 	}
 	
 	@Bean
-	public ApplicationRunner calculationRunner(Calculator calculator) {
-		return args -> {
-			calculator.calcurate(137, 21, '+');
-			calculator.calcurate(137, 21, '*');
-		};
+	public ApplicationRunner calculationRunner(Calculator calculator,
+			@Value("${lhs}") int lhs,
+			@Value("${rhs}") int rhs,
+			@Value("${op}") char op) {
+		return args -> calculator.calcurate(lhs, rhs, op);
 	}
 
 }
