67bb629c840696d0c5a3e993673d191633966bdf
Cyaegha96 <zazae51@gmail.com>
2021-11-18 15:59:56 +0900

    thymeleaf

--- src/main/java/springboot/Controller/BookController.java
+++ src/main/java/springboot/Controller/BookController.java
@@ -4,19 +4,21 @@ package springboot.Controller;
 import java.net.URI;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import springboot.Book;
 import springboot.BookService;
 
-@RestController
-@RequestMapping("/books")
+@Controller
 public class BookController {
 	
 	private final BookService bookService;
@@ -25,20 +27,16 @@ public class BookController {
 		this.bookService = bookService;
 	}
 	
-	@GetMapping
-	public Iterable<Book> all(){
-		return bookService.findAll();
+	@GetMapping("/books.html")
+	public String all(Model model){
+		model.addAttribute("books", bookService.findAll());
+		return "books/list";
 	}
 	
-	@GetMapping("/{isbn}")
-	public ResponseEntity<Book> get(@PathVariable("isbn") String isbn){
-		return bookService.find(isbn).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+	@GetMapping(value ="/books.html", params="isbn")
+	public String get(@RequestParam("isbn") String isbn, Model model) {
+		bookService.find(isbn).ifPresent(book -> model.addAttribute("book", book));
+		return "books/details";		
 	}
 	
-	@PostMapping
-	public ResponseEntity<Book> create(@RequestBody Book book, UriComponentsBuilder uriBuilder){
-		Book created = bookService.create(book);
-		URI newBookUri = uriBuilder.path("/books/{isbn}").build(created.getIsbn());
-		return ResponseEntity.created(newBookUri).body(created);
-	}
 }	
